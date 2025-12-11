2c89e4662d4ea06d305d4611268cd5ae4eb86362
Meron Fikru <fikru@usc.edu>
2025-12-11 06:54:28 +0000

    complete wordle and schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+//bool scheduleHelper(size_t day, size_t worker, DailySchedule& sched, const AvailabilityMatrix& avail, vector<int>& shiftsWorked, const size_t maxShifts, const size_t dailyNeed);
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsWorked, size_t day, size_t pos);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +39,133 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t n = avail.size();
+    if (n == 0){
+      return false;
+    }
 
+    size_t k = avail[0].size();
+    sched.assign(n, vector<Worker_T>(dailyNeed, INVALID_ID));
+    vector<size_t> shiftsWorked(k,0);
+    //return scheduleHelper(0, 0, sched, avail, shiftsWorked, maxShifts, dailyNeed);
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsWorked, 0, 0);
 
 
 }
+
+
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsWorked, size_t day, size_t pos)
+{
+    if(day == avail.size()){
+      return true;
+    }
+    if(pos == dailyNeed){
+      return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsWorked, day+1, 0);
+    }
+
+    for(size_t worker = 0; worker < avail[0].size(); worker++){
+        if(avail[day][worker] && shiftsWorked[worker] < maxShifts){
+            bool workedAlr = false;
+            for(size_t i = 0; i < pos; i++){
+                if(sched[day][i] == worker){
+                    workedAlr = true;
+                    break;
+                }
+            }
+            if (workedAlr == false){
+              sched[day][pos] = worker;
+              shiftsWorked[worker]++;
+              if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsWorked, day, pos+1)){
+                return true;
+              }
+              shiftsWorked[worker]--;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+/*
+bool scheduleHelper(size_t day, size_t worker, DailySchedule& sched, const AvailabilityMatrix& avail, vector<int>& shiftsWorked, const size_t maxShifts, const size_t dailyNeed){
+    if (day == sched.size()){
+        return true; 
+    }
+    else{
+      size_t amountWorkers = avail.size();
+      for (size_t i = 0; i < amountWorkers; i++){
+        if (avail[day][worker]== false){}
+        else{
+          if (shiftsWorked[i]< maxShifts){
+            bool WorkedAlr = false; 
+            for (size_t j = 0; j<worker; j++){
+              if(sched[day][j]== i){
+                WorkedAlr =true;
+                break;
+              }
+            }
+            if (WorkedAlr == false){
+              sched[day][worker] = i;
+              shiftsWorked[i]++;
+
+              bool valid;
+              if (worker+1 < dailyNeed){
+                valid = scheduleHelper(day,worker+1,sched,avail,shiftsWorked, maxShifts, dailyNeed);
+              }
+              else{
+                valid = scheduleHelper(day,0,sched,avail,shiftsWorked, maxShifts, dailyNeed);
+                
+              }
+              if (valid == true){
+                return true; 
+              }
+
+              sched[day][worker] = INVALID_ID;
+              shiftsWorked[i]--;
+            }
+          } 
+        }
+      }
+      return false;
+      
+        //PREV USED PORTION
+        for (size_t i = 0; i < avail[0].size(); i++ ){
+            if (avail[day][i] == 1){
+                sched[day][i] = 1;
+                shiftsWorked[i]++;
+                if (RecMet(dailyNeed, maxShifts, shiftsWorked, day, workers)){
+                    return scheduleHelper(day+1, unfilledMatrix, avail, shiftsWorked );
+                }
+                else{
+                    unfilledMatrix[day][i] = 0;
+                    shiftsWorked[i]--;
+                }
+            }
+        }
+        return false;
+        //PREV USED PORTION
+    //}
+//}
+
+
+// PREV USED PORTION W SECOND HELPER
+  for (size_t i = 0; i < avail[0].size(); i++ ){
+    if (avail[day][i] == 1){
+      sched[day][i] = 1;
+      shiftsWorked[i]++;
+      if (RecMet(dailyNeed, maxShifts, shiftsWorked, day, workers)){
+        return scheduleHelper(day+1, unfilledMatrix, avail, shiftsWorked );
+      }
+      else{
+        unfilledMatrix[day][i] = 0;
+        shiftsWorked[i]--;
+          }
+        }
+      }
+    return false;
+  }
+*/
+    
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void validWords(string combo, string floatingCombo, string ogInput, set<string>& valid, const set<string>& dict);
+string floatingAndDashes(string ogInput, string floating);
+void floatStringGen(string floating, string output, set<string>& combos);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,98 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> valid;
+    set<string> combos; 
+    string ogString = in;
+    string floatingLetters = floating;
+    string output = "";
+    string combo = "";
 
+    string floatingWDashes = floatingAndDashes(ogString, floatingLetters);
+    
+    floatStringGen(floatingWDashes, output, combos);
+
+    //cout << "float string gen finished" << endl;
+
+    for (string element : combos){
+        //cout << "CURRENT COMBO: " << element << endl; 
+        validWords(combo, element, ogString, valid, dict);
+    }
+
+    return valid; 
+    
+  
 }
 
 // Define any helper functions here
+//takes a single floating combo and sees if it can be a validWord
+void validWords(string combo, string floatingCombo, string ogInput, set<string>& valid, const set<string>& dict){
+    if (ogInput.empty()){
+        auto it = dict.find(combo);
+        if (it != dict.end()){
+            //cout << "VALID WORD TO INSERT IN VALID " << combo <<endl; 
+            valid.insert(combo);
+        }
+        return;
+    }
+    else{
+        string newFloatingCombo = floatingCombo;
+        string newOgInput = ogInput;
+
+        if (ogInput[0]!= '-'){
+            newOgInput.erase(0,1);
+            validWords(combo + ogInput[0], floatingCombo, newOgInput, valid, dict);
+        }
+        else {
+            if (ogInput[0]== '-'){
+                if (floatingCombo[0]=='-'){
+                    newOgInput.erase(0,1);
+                    newFloatingCombo.erase(0,1);
+                    for (char letter = 'a'; letter <= 'z'; ++letter){
+                        validWords(combo + letter, newFloatingCombo, newOgInput, valid, dict);
+                    }
+                }
+                else{
+                newOgInput.erase(0,1);
+                newFloatingCombo.erase(0,1);
+                validWords(combo + floatingCombo[0], newFloatingCombo, newOgInput, valid, dict);
+            
+                }
+            }
+            
+        }
+    }
+}
+
+
+void floatStringGen(string floating, string output, set<string>& combos)
+{
+    if (floating.empty()== true){
+        //cout << output << endl;
+        combos.insert(output);
+        return;
+    }
+    else{
+        for (size_t i=0; i<floating.size(); i++){
+            char letter = floating[i];
+            string newFloating = floating;
+            newFloating.erase(i,1);
+            floatStringGen(newFloating, output + letter, combos);
+        }
+    }
+}
+
+// this helper funct should generate the floating for the floatStringGen
+string floatingAndDashes(string ogInput, string floating){
+    size_t dashes = 0;
+    string temp = floating;
+    for(size_t i=0; i<ogInput.size(); i++){
+        if (ogInput[i] == '-'){
+            dashes++;
+        }
+        if (dashes>temp.size() && ogInput[i] == '-'){
+            floating = floating + '-';
+        }
+    }
+    return floating;
+}
